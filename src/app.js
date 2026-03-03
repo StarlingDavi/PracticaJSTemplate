49aed10f27088b6744ef70817d503fac36f9916e
unknown <Starlingdavidherrera@gmail.com>
2026-03-03 08:01:54 -0400

    connect web page to express

--- src/app.js
+++ src/app.js
@@ -2,18 +2,18 @@ import express from "express";
 import { fileURLToPath } from "node:url";
 import path from "path";
 
-const __filename = fileURLToPath(import.meta.env);
+const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
 const port = process.env.port || 3000;
 
 app.set("view engine", "ejs")
-app.set("views", path.join(__dirname, "views"))
 app.use(express.static(path.join(__dirname, "../public")))
+app.set("views", path.join(__dirname, "views"))
 
 app.get("/", (req, res) => {
-    res.send("index")
+    res.render("index")
 })
 
 app.listen(port, () => {
